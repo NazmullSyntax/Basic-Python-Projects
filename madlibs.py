afa045461d8a52ba42fd8cadd6ac8e22d852202c
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-09-10 00:14:19 +0600

    add madlabs game

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+print("Enter the following:")
+adj = input("Adjective: ")
+noun = input("Noun: ")
+verb = input("Verb: ")
+
+story = f"Today I was very {adj}. I saw a {noun} that could {verb}!"
+print(story)
