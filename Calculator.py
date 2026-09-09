21a46b2d069205c7fb1844a06ff0c6d924045281
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-09-10 00:00:52 +0600

    add calculator

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+def calculator():
+    num1 = float(input("Enter first number: "))
+    op = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+    
+    if op == '+':
+        print(num1 + num2)
+    elif op == '-':
+        print(num1 - num2)
+    elif op == '*':
+        print(num1 * num2)
+    elif op == '/':
+        print(num1 / num2 if num2 != 0 else "Cannot divide by zero")
